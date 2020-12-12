037c11db342b5d67c2b1a3bf38245fca7db63b71
Hayk <hayk.hayr3@gmail.com>
2020-12-13 03:35:41 +0400

    Add project files.

---
+++ Less07_task05/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Less07_task05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyClass.MathStruct instance = new MyClass.MathStruct();
+            int square = instance.Square(7);
+
+            Console.ReadKey();
+        }
+    }
+
+    class MyClass
+    {
+        public struct MathStruct
+        {
+            public int Square(int a)
+            {
+                return a * a;
+            }
+        }
+    }
+}
